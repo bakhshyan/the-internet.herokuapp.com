77e7486b52fbcb3863fa1b530ecaaa89af66d2ba
hayk bakhshyan <haykbakhshyan@gmail.com>
2020-09-06 01:38:17 +0400

    new changes

---
+++ src/main/java/pages/EmailSentPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class EmailSentPage {
+    private WebDriver driver;
+    By statusAlert = By.id("content");
+    public EmailSentPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getAlertText() {
+        return driver.findElement(statusAlert).getText();
+    }
+}
---
+++ src/test/java/forgotpassword/ForgotPasswordTest.java
@@ -0,0 +1,17 @@
+package forgotpassword;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ForgotPasswordTest extends BaseTest {
+
+    @Test
+    public void testForgotPassword() {
+        var forgotPasswordPage = homePage.clickForgotPassword();
+        forgotPasswordPage.setEmailField("tau@example.com");
+        var emailSentPage = forgotPasswordPage.clickRetrievePassword();
+        Assert.assertTrue(emailSentPage.getAlertText().contains("Your e-mail's been sent!"),"Alert text is inccorect");
+    }
+
+}
