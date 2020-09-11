1dd3e543da9194248964601ba2492229b06234ac
hayk bakhshyan <haykbakhshyan@gmail.com>
2020-09-11 17:26:30 +0400

    EvenListeners

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -5,23 +5,26 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 import pages.HomePage;
+import utils.EventReporter;
 import utils.WindowManager;
 
 import java.io.File;
 import java.io.IOException;
 
 public class BaseTest {
-    private WebDriver driver;
+    private EventFiringWebDriver driver;
     protected HomePage homePage;
 
     @BeforeClass
     public void setUp() {
 
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new EventReporter());
 //        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         goHome();
         driver.manage().window().maximize();
