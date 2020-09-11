4542689622b259529198a5ec68ebff44c02c85f1
hayk bakhshyan <haykbakhshyan@gmail.com>
2020-09-11 17:07:09 +0400

    adding takingScreenShot functionality

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,16 +1,17 @@
 package base;
 
-import org.openqa.selenium.Dimension;
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 import pages.HomePage;
 import utils.WindowManager;
 
-import java.util.concurrent.TimeUnit;
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTest {
     private WebDriver driver;
@@ -32,6 +33,16 @@ public class BaseTest {
     public void goHome() {
         driver.get("https://the-internet.herokuapp.com/");
 
+    }
+
+    @AfterMethod
+    public void takeScreenShot(ITestResult result) throws IOException {
+        if (ITestResult.FAILURE == result.getStatus()) {
+
+            var camera = (TakesScreenshot) driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            Files.move(screenshot, new File("resources/screenshots/"+result.getName()+".png"));
+        }
     }
 
     @AfterClass
